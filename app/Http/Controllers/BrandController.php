df18e4d0258bdede43833e2c5cbb20c7e512b270
ikdevv <techik502@gmail.com>
2025-04-19 13:24:57 +0530

    brand basic crud implemented

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,44 +4,83 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BrandController extends Controller
 {
 
-    public function index() {}
+    public function index()
+    {
+        $brands = Brand::all();
+        return view('brand.index', compact('brands'));
+    }
 
 
     public function create()
     {
-        //
+        return view('brand.create');
     }
 
 
     public function store(Request $request)
     {
-        //
+
+        $validator = Validator::make($request->all(), [
+            'code' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
+            'status' => 'required|in:active,inactive',
+        ]);
+
+
+        if ($validator->fails()) {
+            return redirect()->back()->with('error', $validator->errors()->first());
+        }
+
+        $brand = new Brand(
+            [
+                'code' => $request->code,
+                'name' => $request->name,
+                'status' => $request->status,
+            ]
+        );
+
+        $brand->save();
+
+        return redirect()->route('brand.index')->with('success', 'Brand created successfully');
     }
 
 
-    public function show(Brand $brand)
-    {
-        //
-    }
+    public function show(Brand $brand) {}
 
 
     public function edit(Brand $brand)
     {
-        //
+        $brand = Brand::find($brand->id);
+        return view('brand.edit', compact('brand'));
     }
 
     public function update(Request $request, Brand $brand)
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'status' => 'required|in:active,inactive',
+        ]);
 
+        if ($validator->fails()) {
+            return redirect()->back()->with('error', $validator->errors()->first());
+        }
+
+
+        $brand->update([
+
+            'name' => $request->name,
+            'status' => $request->status,
+        ]);
+    }
 
     public function destroy(Brand $brand)
     {
-        //
+        $brand->delete();
+        return redirect()->back()->with('success', 'Brand deleted successfully');
     }
 }
